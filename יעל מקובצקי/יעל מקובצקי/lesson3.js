e94503b9b531bd1269a9d15dfd7259741accc13e
חוי קונינסקי <ChaviKoninsky@smg.local>
2025-11-11 11:57:00 +0200

    HW_ChaviK_Yael

--- יעל מקובצקי/יעל מקובצקי/lesson3.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let data=fs.readFileSync('file.json','utf8');
-fs.writeFileSync('file.json',data+',\n{"first name":"Ayala",\n"last name":"mako"}')
-console.log(fs.readFileSync('file.json','utf8'))
